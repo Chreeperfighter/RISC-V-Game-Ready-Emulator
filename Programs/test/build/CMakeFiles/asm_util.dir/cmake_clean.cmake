050ad3bba1191199667a37be727773a306d4c58c
Chreeperfighter <markv1709@gmail.com>
2025-11-20 13:10:19 +0100

    chore: clean repo and apply new .gitignore rules

--- Programs/test/build/CMakeFiles/asm_util.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/asm_util"
-  "util.s"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/asm_util.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Programs/test/build/CMakeFiles/asm_util.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for asm_util.
-# This may be replaced when dependencies are built.
